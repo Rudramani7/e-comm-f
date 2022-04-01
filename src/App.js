b5278c82b78c95c8374e082431edfcea2c0525f4
Rudramani7 <rudramani1410@gmail.com>
2022-04-01 08:38:18 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  return(
+    <div className='App'>
+      <PhoneList />
+    </div>
+  )
+
+}
+
+function PhoneList(){
+  const [mobiles, setMobiles] = useState([]);
+
+ useEffect(() => {
+   fetch("http://localhost:4000/mobiles")
+  .then((data) => data.json())
+  .then((mbs) => setMobiles(mbs));
+},[]);
   return (
-  <div></div>
+    <div className='phone-list-container'>
+      {mobiles.map(mobile => (<Phone mobile={mobile} />))}
+    </div>
+ );
+}
+
+function Phone({mobile}){
+  return (
+  <div className='phone-container'>
+    <img src={mobile.img} alt={mobile.model} className='phone-picture' />
+    <h2 className='phone-name'>{mobile.model}</h2>
+    <p className='phone-company'>{mobile.company}</p>
+  </div>
   );
 }
 
